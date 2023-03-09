4b0e538a2dca69adbd86e8de085383ef4fcbb221
Bromic Ouma <oumabromic7@gmail.com>
2023-03-09 05:00:07 -0800

    Print list of arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     """
     length = (len(argv))
     if length == 1:
-        print("{:d} arguments.".format(length - 1))
+        print("{:d} arguments .".format(length - 1))
     elif length == 2:
         print("{:d} argument:".format(length - 1))
         print("{:d}: {} ".format((length - 1), argv[1]))
