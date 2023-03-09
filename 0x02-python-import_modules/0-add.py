280bd1c9b69f39d0b5a6e1140a01064acd739b91
Bromic Ouma <oumabromic7@gmail.com>
2023-03-09 02:11:56 -0800

    Sum of 2 numbers

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+import add_0
+
+if __name__ == "__main__":
+    """
+
+    prints the addition of two numbers
+
+    """
+
+    a = 1
+    b = 2
+
+    result = add_0.add(a, b)
+    print("{} + {} = {}".format(a, b, result))
