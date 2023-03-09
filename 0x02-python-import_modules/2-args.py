8d23e8b8ac063830ac7c2c3d2b857d29202e9b69
Bromic Ouma <oumabromic7@gmail.com>
2023-03-09 05:06:33 -0800

    String format specifier added

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,11 +9,11 @@ if __name__ == "__main__":
     """
     length = (len(argv))
     if length == 1:
-        print("{:d} arguments .".format(length - 1))
+        print("{:d} arguments.".format(length - 1))
     elif length == 2:
         print("{:d} argument:".format(length - 1))
-        print("{:d}: {} ".format((length - 1), argv[1]))
+        print("{:d}: {:s} ".format((length - 1), argv[1]))
     else:
         print("{:d} arguments:".format(length - 1))
         for i in range(length - 1):
-            print("{:d}: {}".format((i + 1), argv[i + 1]))
+            print("{:d}: {:s}".format((i + 1), argv[i + 1]))
