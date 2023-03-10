72cea4b06862bfc4febbe033a5c07ab3dd24baa1
Bromic Ouma <oumabromic7@gmail.com>
2023-03-10 08:35:47 -0800

    Print list

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/pyhthon3
+
+def print_list_integer(my_list=[]):
+    """ print integers of a list
+     my_list: lists
+
+    """
+    for i in range(len(my_list)):
+        print("{:d}".format(my_list[i]))
