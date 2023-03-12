7c6869f4f15dedfc91b901d1abd69822f0181947
Bromic Ouma <oumabromic7@gmail.com>
2023-03-12 03:27:19 -0700

    Print list in reverse first che if empty

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
-    length = len(my_list)-1
-    while length >= 0:
-        print("{}".format(my_list[length]))
-        length -= 1
+    length = len(my_list)
+    if my_list:
+        idx = length - 1
+        while idx >= 0:
+            print("{}".format(my_list[idx]))
+            idx -= 1
