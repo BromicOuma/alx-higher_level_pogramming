f8fdce22f71dddc9ee47a6c359498035c3714726
Bromic Ouma <oumabromic7@gmail.com>
2023-03-15 08:53:28 -0700

    Square f iterables of list

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
+    if len(matrix) > 0:
+        for rows in matrix:
+            new_matrix.append(list(map(lambda x: x ** 2, rows)))
+    return new_matrix
