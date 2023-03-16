4ec8859b006386ec34ca55ca741f87022fb73155
Bromic Ouma <oumabromic7@gmail.com>
2023-03-16 03:59:23 -0700

    Search and replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,5 @@
-#!/usr/bin/python3
-
+#!/usr/bin/pytthon3
 def search_replace(my_list, search, replace):
-    if my_list and search and replace:
-        new_list = [replace if x == search else x for x in my_list]
-        return new_list
+
+        new_list = [replace if x == search  else x for x in my_list]
+        return(new_list)
