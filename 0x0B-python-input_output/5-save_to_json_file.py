bed74dd9bf0bc8e097a15b83d8a1f04235086399
Bromic Ouma <oumabromic7@gmail.com>
2023-04-12 14:51:58 -0700

    save to json file

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""writes an Object to a text file,
+   using a JSON representation:"""
+from json import dumps
+
+
+def save_to_json_file(my_obj, filename):
+    """save to json file """
+    with open(filename, mode='w', encoding='utf-8') as f:
+        f.write(dumps(my_obj))
