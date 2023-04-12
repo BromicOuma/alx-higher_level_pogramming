f2f4b073aea51cfda83f4ca55d532e168d5bac22
Bromic Ouma <oumabromic7@gmail.com>
2023-04-12 06:17:34 -0700

    pycode doc

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """this module reads a file encoded in UTF8 """
 
+
 def read_file(filename=""):
     """read file print to stdout """
     with open(filename, "r", encoding="utf-8") as fp:
