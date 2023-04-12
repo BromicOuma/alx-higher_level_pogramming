4077cbcc204f5e8b0e94cea81bda9a29edfcc85c
Bromic Ouma <oumabromic7@gmail.com>
2023-04-12 06:57:22 -0700

    write from std to file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-"""module to write to a file, it creates one of it doesnt exist amd overwrites the contents """
+"""module to write to a file, it creates one if
+   it doesnt exist and overwrites the contents """
 
 
 def write_file(filename="", text=""):
     """write to a file from stdio """
     with open(filename, "w", encoding="utf-8") as fp:
-        return (fp.write(text))#
+        return (fp.write(text))
