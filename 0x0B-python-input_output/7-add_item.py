b7ab9c05d7304c0ccaba170c52cb1e1d4538ac88
Bromic Ouma <oumabromic7@gmail.com>
2023-04-13 04:51:08 -0700

    add item tona list

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """save json to a list """
-
-
 from os import path
 from sys import argv
+
+
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
