a9245db52246b1e86e405eeaf68397306e2ce1e9
Bromic Ouma <oumabromic7@gmail.com>
2023-05-02 00:59:41 -0700

    Checks superclass

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -3,6 +3,13 @@
 
 
 def inherits_from(obj, a_class):
+    """inherits_from checks if the object inherits
+    obj:
+    a_class:
+
+    returns:True or Fals
+    """
+
     if isinstance(obj, a_class) and \
        issubclass(a_class, obj.__class__) is False:
         return True
