a013b9460fe73e26b7332df7fe72e505271037aa
Bromic Ouma <oumabromic7@gmail.com>
2023-06-18 07:28:29 -0700

    script that takes in arguments and displays all values in the states table of hbtn_0e_0_usa where name matches the argument. But this time, write one that is safe from MySQL injections!

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""script that takes in arguments and displays all
+    values in the states table of hbtn_0e_0_usa where
+    name matches the argument. But this time, write one
+    that is safe from MySQL injections!"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    """main execution point safe from mysql injection"""
+
+    conn = MySQLdb.connect(host='localhost', port=3306, passwd=argv[2],
+                           user=argv[1], db=argv[3])
+
+    with conn.cursor() as cur:
+        cur.execute("""
+            SELECT
+                *
+            FROM
+                states
+            WHERE
+                name LIKE BINARY %(name)s
+            ORDER BY states.id ASC """, {'name': argv[4]})
+
+        rows = cur.fetchall()
+
+    if rows is not None:
+        for row in rows:
+            print(row)
